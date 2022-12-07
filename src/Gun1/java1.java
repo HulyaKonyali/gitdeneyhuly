5081fde385b520e034aba9f65fc57a15b09a2314
hulya <konyali.hly@gmail.com>
2022-12-07 22:00:11 +0100

    mer1

--- src/Gun1/java1.java
+++ src/Gun1/java1.java
@@ -5,6 +5,9 @@ public class java1 {
 
     public static void main(String[] args) {
         System.out.println(" merhaba1");
-        System.out.println(" merhaba2");
+        System.out.println(" merhaba2")
+
+        ;
+        System.out.println(" mer 1");
     }
 }
